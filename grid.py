1f09a08f033f05ca73d1a9dc9416841cafe7fdc6
Ott Oopkaup <ott.oopkaup@gmail.com>
2015-10-05 15:08:29 +0300

    Scroll
    Weird scrolling behavior researched

--- grid.py
+++ grid.py
@@ -1,4 +1,6 @@
-tileSize = 25
+
+#Var tileSize hold the current tile size, used in rendering
+tileSize = 30
 import pygame
 import textures
 def getGrid(filename):
@@ -11,7 +13,7 @@ def getGrid(filename):
         newGrid.append(list(line))
     return newGrid
 
-
+#Draws one grid tile at coordinates, on var=screen with color
 def drawGridTile(x,y,screen,tileSize,color,display=False):
     tile = pygame.Rect(x*tileSize,y*tileSize,tileSize,tileSize)
     pygame.draw.rect(screen,color,tile)
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ print(map)
 pygame.init()
 displayInfo = pygame.display.Info()
 screen = pygame.display.set_mode((displayInfo.current_w, displayInfo.current_h))
+screen = pygame.display.set_mode((600,800))
 screen.fill(textures.purple)
 pygame.display.flip()
 
@@ -17,18 +18,32 @@ pygame.display.flip()
 for i in range((displayInfo.current_w)//grid.tileSize):
     for j in range((displayInfo.current_h)//grid.tileSize):
         grid.drawGridTile(i,j,screen,grid.tileSize,[random.randint(0,255),random.randint(0,255),random.randint(0,255)],True)
-     
 pygame.display.flip()
 
+
 Exit = False
+x = 0
+y = 0
 while not Exit:
-    #Esc viskab pygaest välja
+    #Esc viskab pygamest välja
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             Exit = True
         elif i.type == pygame.KEYDOWN:
             if i.key == pygame.K_ESCAPE:
                 Exit = True
-            elif i.key == pygame.K_a:
+            elif i.key == pygame.K_f:
                 pygame.display.toggle_fullscreen()
+    key = pygame.key.get_pressed()
+    if key[pygame.K_w]:
+        y-=1
+    elif key[pygame.K_a]:
+        x-=1
+    elif key[pygame.K_s]:
+        y+=1
+    elif key[pygame.K_d]:
+        x+=1
+    screen.scroll(x,y)
+    pygame.display.flip()
+    x,y=0,0
 pygame.quit()
