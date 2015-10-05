804eec306b20741241ea15bceaa917cbd1e9c9d6
Ott Oopkaup <ott.oopkaup@gmail.com>
2015-10-05 19:49:49 +0300

    Fixed screen clip
    bugfix

--- main.py
+++ main.py
@@ -3,20 +3,25 @@ import sys
 import textures
 import random
 import grid
+map = grid.getGrid("grid.txt")
 
-# map = grid.getGrid("grid.txt")
 
 pygame.init()
 displayInfo = pygame.display.Info()
 screen = pygame.display.set_mode((displayInfo.current_w, displayInfo.current_h))
-pygame.display.set_caption("Grand Theft Python")  # Akna nimesilt.
 screen.fill(textures.purple)
 pygame.display.flip()
 
-# Monitor info:
-print(displayInfo.current_w, displayInfo.current_h)
+#startpos
+pxpos = 500*grid.tileSize//2
+pypos = 500*grid.tileSize//2
+
+
+
+#katsetused gridi joonistamiseks, AJUTINE
 
 checkers = grid.genCheckers()
+
 
 Exit = False
 x = 0
@@ -31,29 +36,21 @@ while not Exit:
                 Exit = True
             elif i.key == pygame.K_f:
                 pygame.display.toggle_fullscreen()
-
     key = pygame.key.get_pressed()
     if key[pygame.K_w]:
-        y += 5
+        y-=1
     elif key[pygame.K_s]:
-        y -= 5
+        y+=1
     if key[pygame.K_a]:
-        x += 5
+        x-=1
     elif key[pygame.K_d]:
-        x -= 5
-
-
-    for i in range((displayInfo.current_w) // grid.tileSize):
-        for j in range((displayInfo.current_h) // grid.tileSize):
-            if checkers[i + x // grid.tileSize][j + y // grid.tileSize] == 0:
-                grid.drawGridTile(i * grid.tileSize + x % grid.tileSize, j * grid.tileSize + y % grid.tileSize,screen, grid.tileSize, textures.silver)
+        x+=1
+    for i in range((displayInfo.current_w)//grid.tileSize+4):
+        for j in range((displayInfo.current_h)//grid.tileSize+4):
+            if checkers[i+x//grid.tileSize][j+y//grid.tileSize] == 0:
+                grid.drawGridTile(i*grid.tileSize+x%grid.tileSize-2*grid.tileSize,j*grid.tileSize+y%grid.tileSize-2*grid.tileSize,screen,grid.tileSize,textures.black)
             else:
-                grid.drawGridTile(i * grid.tileSize + x % grid.tileSize, j * grid.tileSize + y % grid.tileSize,screen, grid.tileSize, textures.white)
-
-    # Player model ekraani keskele.
-    playerModel = pygame.Rect(displayInfo.current_w // 2, displayInfo.current_h // 2, 50, 50)
-    pygame.draw.rect(screen, textures.black, playerModel)
-
+                grid.drawGridTile(i*grid.tileSize+x%grid.tileSize-2*grid.tileSize,j*grid.tileSize+y%grid.tileSize-2*grid.tileSize,screen,grid.tileSize,textures.white)
     pygame.display.flip()
 """
 screen.scroll(x,y)
