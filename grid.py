80f5a8cf1e3854341a2584a654d78787d3f38a25
Oja95 <tiit.oja@gmail.com>
2015-10-07 22:55:07 +0300

    Saab ringi liikuda mapil player modeliga. Clipping bug.

--- grid.py
+++ grid.py
@@ -1,20 +1,9 @@
-
-#Var tileSize hold the current tile size, used in rendering
-tileSize = 50
-
 from PIL import Image
 import pygame
 import textures
 
-def getGrid(filename):
-    f = open(filename)
-    grid = f.read()
-    grid = grid.replace(" ", "")
-    grid = grid.split("\n")
-    newGrid = []
-    for line in grid:
-        newGrid.append(list(line))
-    return newGrid
+#Var tileSize hold the current tile size, used in rendering
+tileSize = 7
 
 #Draws one grid tile at coordinates, on var=screen with color
 def drawGridTile(x,y,screen,tileSize,color):
@@ -38,23 +27,20 @@ def getPixelsFromImage(file):
     pixel_values = list(im.getdata())
     pixelList = []
     aPixel = []
-    width = 2
-    height = 3
     for i in range(width*height):
         for j in range(3):
             aPixel.append(pixel_values[i][j])
         pixelList.append(aPixel)
         aPixel = []
     # Pikslid vaja saada 2d arraysse width * height
+
     pixel2dList = []
     counter = 0
-    for i in range(width):
+    for i in range(height):
         pixel2dList.append([])
-        for j in range(height):
+        for j in range(width):
             pixel2dList[i].append(pixelList[counter])
             counter += 1
-    f = open("maptxt.txt", "w")
-    f.write(str(pixel2dList))
-
     return pixel2dList
-getPixelsFromImage("map.tiff")
+
+# pixel2dlist kujul list[kõrgus][laius]
--- main.py
+++ main.py
@@ -16,7 +16,8 @@ pygame.display.flip()
 print(displayInfo.current_w, displayInfo.current_h)
 # Loeme pildifailist pikslid
 
-checkers = grid.genCheckers()
+#checkers = grid.genCheckers()
+checkers = grid.getPixelsFromImage("map.tiff")
 
 Exit = False
 x = 0
@@ -34,37 +35,34 @@ while not Exit:
 
     key = pygame.key.get_pressed()
     if key[pygame.K_w]:
-        y += 5
-    elif key[pygame.K_s]:
         y -= 5
+    elif key[pygame.K_s]:
+        y += 5
     if key[pygame.K_a]:
-        x += 5
-    elif key[pygame.K_d]:
         x -= 5
-
-
+    elif key[pygame.K_d]:
+        x += 5
 
     for i in range((displayInfo.current_w) // grid.tileSize + 4):
         for j in range((displayInfo.current_h)//grid.tileSize + 4):
             X = (i-2) * grid.tileSize + x % grid.tileSize
             Y = (j-2) * grid.tileSize + y % grid.tileSize
-            if checkers[i + x // grid.tileSize][j + y // grid.tileSize] == 0:
+            if checkers[i + x // grid.tileSize][j + y // grid.tileSize] == [0, 0, 0]:
                 grid.drawGridTile(X, Y, screen, grid.tileSize, textures.black)
-            else:
+            elif checkers[i + x // grid.tileSize][j + y // grid.tileSize] == [255, 255, 255]:
                 grid.drawGridTile(X, Y, screen, grid.tileSize, textures.white)
+            else:
+                grid.drawGridTile(X, Y, screen, grid.tileSize, textures.green)
 
     # Player model ekraani keskele.
-    playerModel = pygame.Rect(displayInfo.current_w // 2, displayInfo.current_h // 2, 30, 20)
-    pygame.draw.rect(screen, textures.green, playerModel)
+
+    playerModel = pygame.Rect(displayInfo.current_w // 2, displayInfo.current_h // 2, 50 ,50 )
+    pygame.draw.rect(screen, textures.blue, playerModel)
 
     pygame.display.flip()
 
     endTime = time.time()
-    print(1/(endTime-startTime))
-"""
-screen.scroll(x,y)
-pygame.display.flip()
-x,y=0,0
-"""
+    #print(1/(endTime-startTime))
+
 
 pygame.quit()
